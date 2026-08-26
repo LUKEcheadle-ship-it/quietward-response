d8c3338b280cc867d065eedf033dd40c504e83a4
LUKEcheadle-ship-it <LUKEcheadle@icloud.com>
2026-08-25 22:08:09 -0500

    Package hardened agent state helper on Linux

--- scripts/install_response_agent_user_service.sh
+++ scripts/install_response_agent_user_service.sh
@@ -39,6 +39,7 @@ runtime_files=(
   response_agent_file_v12.py
   response_agent_network.py
   response_agent_resources.py
+  private_state_io.py
 )
 for file in "${runtime_files[@]}"; do
   install -m 700 "${repo_root}/scripts/${file}" "${install_root}/${file}"
